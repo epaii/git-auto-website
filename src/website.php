e18dabd5780c1353b2ca953ad26c7d683c1255e2
MrRen <543169072@qq.com>
2021-10-19 14:32:26 +0800

    rlr

--- src/website.php
+++ src/website.php
@@ -20,8 +20,12 @@ class website{
             self::runCmd(self::$git."  clone ".$gitDir." ".$webDir);
             file_put_contents($hook = $gitDir.DIRECTORY_SEPARATOR."hooks".DIRECTORY_SEPARATOR."post-update",
             '#!/bin/sh
+            php /webs/git-auto-website/hook.php start '.$webDir.'
             if [ -f "/epii/hooks/git.sh" ]; then
                 /epii/hooks/git.sh start '.$webDir.'
+            fi
+            if [ -f "'.$webDir.'/git-post-update.php" ]; then
+                php '.$webDir.'/git-post-update.php start
             fi
             DEPLOY_PATH='.$webDir.'
             unset GIT_DIR
@@ -29,8 +33,12 @@ class website{
             git fetch --all
             git reset --hard origin/master
             git pull
+            php /webs/git-auto-website/hook.php finish '.$webDir.'
             if [ -f "/epii/hooks/git.sh" ]; then
                 /epii/hooks/git.sh finish '.$webDir.'
+            fi
+            if [ -f "'.$webDir.'/git-post-update.php" ]; then
+                php '.$webDir.'/git-post-update.php finish
             fi
             exec git update-server-info'
             );
